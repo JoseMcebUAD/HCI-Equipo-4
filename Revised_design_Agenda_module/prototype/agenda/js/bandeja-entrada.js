91ff81d7f0261e849fe30ac87ebc210ed8813aab
jose ceballos <a20201556@alumnos.uady.mx>
2026-04-27 02:53:24 -0600

    fix: files not necesary for this delivery

--- Revised_design_Agenda_module/prototype/agenda/js/bandeja-entrada.js
+++
@@ -1,5 +0,0 @@
-/* ============================================================
-   bandeja-entrada.js – DEPRECATED
-   La funcionalidad de bandeja de entrada fue reemplazada por
-   el mini-calendario estilo Teams en agenda.js v3.
-   ============================================================ */
